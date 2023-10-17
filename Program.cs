cebcc9f61de000864b2771375480a08b5d7a9a66
Dell-User <Dell-User@DESKTOP-M748B2N>
2023-10-17 05:15:58 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Numbers_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(sumTriple(1, 2));
+            Console.WriteLine(sumTriple(3,2));
+            Console.WriteLine(sumTriple(2,2));
+            Console.ReadKey();
+        }
+
+      
+        public static int sumTriple(int x, int y)
+        {
+            return x == y ? (x + y)*3 : x + y;
+        }
+       
+    }
+}
